b308e0831326fd9142f04140d8213a9835654129
Imad Mashhood <mashhoodimad@gmail.com>
2022-04-10 18:46:44 -0500

    Part 82 Switch to Constraint Layout

--- app/src/main/java/us/rushbmarketing/profilepage/ProfilePage.kt
+++ app/src/main/java/us/rushbmarketing/profilepage/ProfilePage.kt
@@ -20,6 +20,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 
 @Composable
 fun ProfilePage(){
@@ -27,12 +29,12 @@ fun ProfilePage(){
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 100.dp, bottom = 10.dp, start = 16.dp, end = 16.dp)
-            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(30.dp))
+            //.border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(30.dp))
     ) {
-        Column(Modifier.verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
+        ConstraintLayout {
+            val(image, nameText, aboutText, rowStats,
+                buttonFollow, buttonMessage) = createRefs()
+            val guideLine =createGuidelineFromTop(0.02f)
             Image(painter = painterResource(id = R.drawable.imad),
                 contentDescription = "Imad Profile Picture",
                 modifier = Modifier
@@ -42,31 +44,61 @@ fun ProfilePage(){
                         width = 2.dp,
                         color = Color.Red,
                         shape = CircleShape
-                    ),
+                    ).constrainAs(image){
+                        top.linkTo(guideLine)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
                 contentScale = ContentScale.Crop
             )
-            Text(text="Imad Mashhood")
-            Text(text = "Katy, TX")
+            Text(text="Imad Mashhood",
+                    modifier = Modifier.constrainAs(nameText){
+                        top.linkTo(image.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                )
+
+            Text(text = "Katy, TX",
+                modifier = Modifier.constrainAs(aboutText){
+                    top.linkTo(nameText.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+
             Row(horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
+                    .constrainAs(rowStats){
+                        top.linkTo(aboutText.bottom)
+
+                    }
             ) {
                 ProfileStats(amnt = "48", stat = "Posts")
                 ProfileStats(amnt = "617", stat = "Followers")
                 ProfileStats(amnt = "342", stat = "Following")
             }
-            Row(horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ){
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Follow")
+            Button(onClick = { /*TODO*/ },
+            modifier = Modifier.constrainAs(buttonFollow){
+                top.linkTo(rowStats.bottom, margin = 16.dp)
+                start.linkTo(parent.start)
+                end.linkTo(buttonMessage.start)
+                width= Dimension.wrapContent
+            }
+            ) {
+                Text(text = "Follow")
+            }
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier.constrainAs(buttonMessage){
+                    top.linkTo(rowStats.bottom, margin = 16.dp)
+                    start.linkTo(buttonFollow.end)
+                    end.linkTo(parent.end)
+                    width= Dimension.wrapContent
                 }
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Message")
-                }
+            ) {
+                Text(text = "Message")
             }
         }
     }
